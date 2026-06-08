a8f46459eb6313cec62d1ee7caaf2e225ed0f1c0
EveKennedy <evek092@gmail.com>
2026-06-08 16:15:09 +0100

    Remove duplicate chord heading

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.querySelectorAll(".chord-button").forEach((button) => {
   button.addEventListener("click", () => {
     document.querySelector(".chord-button.active")?.classList.remove("active");
     button.classList.add("active");
-    document.querySelector("#chord-title").textContent = button.dataset.title;
     document.querySelector("#chord-fact").textContent = button.dataset.fact;
   });
 });
